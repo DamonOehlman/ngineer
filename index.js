0fd5972a9ec1a45b2c47dcf5453ab635571cd9af
Damon Oehlman <damon.oehlman@sidelab.com>
2012-11-12 14:39:43 +1000

    Adding logic for reading PID file and reporting nginx process status

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var fs = require('fs'),
+var debug = require('debug')('ngineer'),
+    fs = require('fs'),
     path = require('path'),
     events = require('events'),
     util = require('util'),
+    procinfo = require('procinfo'),
     NginxLocation = require('./lib/location');
 
-function Ngineer(basePath) {
+function Ngineer(basePath, opts) {
     events.EventEmitter.call(this);
+
+    // ensure we have opts
+    opts = opts || {};
+
+    // initialise the basepath to the path provided
+    this.basePath = path.resolve(basePath);
 
     // initialise to not online
     this.online = false;
+
+    // setup monitoring of the pid file
+    this._readPID(opts);
     // create a file system watcher for the pid file
     // this.watcher = fs.watch(path.resolve(basePath, ))
+
+    // look for t
 }
 
 util.inherits(Ngineer, events.EventEmitter);
@@ -28,6 +41,26 @@ Ngineer.prototype.reload = function() {
     // TODO: send the signal
 
     return this;
+};
+
+/* internal methods */
+
+Ngineer.prototype._readPID = function(opts) {
+    var pidLocation = path.resolve(this.basePath, opts.pidFile || 'logs/nginx.pid');
+
+    // open the pid file
+    fs.readFile(pidLocation, 'utf8', function(err, data) {
+        // if we hit an error, then monitor the folder for changes - looking for the pid
+        if (err) {
+
+        }
+        else {
+            debug('looking up process information for process: ' + data);
+            procinfo(parseInt(data, 10), function(err, processData) {
+                console.log(processData);
+            });
+        }
+    });
 };
 
 
