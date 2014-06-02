bd4b5e1402175e8b633cce8359c1def3715b66f3
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-02 15:57:25 +1000

    Added examples directory

---
+++ examples/start.js
@@ -0,0 +1,12 @@
+var async = require('async');
+var nginx = require('..')(__dirname + '/demo', {
+  port: 8080
+});
+
+async.series([ nginx.scaffold, nginx.start ], function(err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('started nginx');
+});
