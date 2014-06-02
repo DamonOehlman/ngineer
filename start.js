6cc6d34c520ec63632777c2c675dd847dcb839f2
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-02 15:56:49 +1000

    Improved start resilience

--- start.js
+++ start.js
@@ -3,16 +3,28 @@ var debug = require('debug')('ngineer:start');
 var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
-
-var NGINX_EXECUTABLES = [
-  path.resolve('nginx'),
-  '/usr/sbin/nginx'
-];
+var config = require('./config');
 
 module.exports = function(ngineer, basePath, opts) {
+  var startTimeout;
+  var command;
+
+  function nginxStart(callback) {
+    return function() {
+      debug('running: ' + command);
+      exec(command, function(err) {
+        debug('started: ', err);
+
+        if (err) {
+          return callback(err);
+        }
+      });
+    }
+  }
+
   return function(callback) {
-    async.filter(NGINX_EXECUTABLES, fs.exists, function(results) {
-      var command = results[0] + ' -p ' + basePath + '/ -c conf/nginx.conf';
+    async.filter(config.executables, fs.exists, function(results) {
+      command = results[0] + ' -p ' + basePath + '/ -c conf/nginx.conf';
 
       if (results.length === 0) {
         return callback(new Error('no nginx executable found'));
@@ -23,17 +35,12 @@ module.exports = function(ngineer, basePath, opts) {
         return callback();
       }
 
-      debug('running: ' + command);
-      exec(command, function(err) {
-        debug('started: ', err);
-
-        if (err) {
-          return callback(err);
-        }
-
-        // monitor for nginx starting
-        ngineer.once('online', callback);
+      ngineer.once('online', function() {
+        clearTimeout(startTimeout);
+        callback();
       });
+
+      startTimeout = setTimeout(nginxStart(callback), 500);
     });
   };
 
